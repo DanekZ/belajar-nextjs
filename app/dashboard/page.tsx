2495e0617b4e72020a4009dd2c0fb653131b13dd
zidane abbas <danezidane93@gmail.com>
2024-02-04 18:40:23 +0800

    ubah karena iseng

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,8 @@
 export default function DashboardPage(){
    return (
       <>
-         <h1>Halaman Dashboard</h1>
+         {/* <h1>Halaman Dashboard</h1> */}
+         <h2 className="text-6xl font-bold">Fadhil Pelernya Kecil</h2>
       </>
    )
 }
